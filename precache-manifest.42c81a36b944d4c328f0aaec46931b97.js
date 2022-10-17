2397d0659f0ae4f1b0fb2af746c1aee77096c931
CorbysReckoning <112131019+CorbysReckoning@users.noreply.github.com>
2022-10-17 16:02:53 -0600

    Add files via upload
    2nd attempt at React Build initiation.

---
+++ precache-manifest.42c81a36b944d4c328f0aaec46931b97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d602d2789d0d8dc9752c",
+    "url": "/static/css/main.2c1ffce6.chunk.css"
+  },
+  {
+    "revision": "d602d2789d0d8dc9752c",
+    "url": "/static/js/main.d602d278.chunk.js"
+  },
+  {
+    "revision": "04a190654ccba05b0606",
+    "url": "/static/js/1.04a19065.chunk.js"
+  },
+  {
+    "revision": "ccd71136542f3612f254",
+    "url": "/static/css/2.d6926742.chunk.css"
+  },
+  {
+    "revision": "ccd71136542f3612f254",
+    "url": "/static/js/2.ccd71136.chunk.js"
+  },
+  {
+    "revision": "83690eb48c6700e2fa4f",
+    "url": "/static/js/runtime~main.83690eb4.js"
+  },
+  {
+    "revision": "332115f8e6d944d4474760ac5dfd6223",
+    "url": "/index.html"
+  }
+];
